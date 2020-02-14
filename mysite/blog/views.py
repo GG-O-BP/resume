212c280c404e5c10f2540361a14e0852ab42cbe8
GGOBP <GGOBP@protonmail.ch>
2020-02-14 11:33:46 +0900

    list view

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,14 +1,9 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
-def index(request):
-    posts = Post.objects.all()
+class PostList(ListView):
+    model = Post
 
-    return render(
-        request,
-        'blog/index.html',
-        {
-            'posts':posts,
-        }
-    )
-
+    def get_queryset(self):
+        return Post.objects.order_by('-created')

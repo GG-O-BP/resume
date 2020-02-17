524d7b875f1d8cbf08cb5876de64c351ffa3735b
GGOBP <GGOBP@protonmail.ch>
2020-02-17 11:02:52 +0900

    detailPage

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 class PostList(ListView):
     model = Post
 
     def get_queryset(self):
         return Post.objects.order_by('-created')
+
+class PostDetail(DetailView):
+    model = Post
+

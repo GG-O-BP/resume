ebc162eef49a34a05c2b76250de921c70b000cf0
GGOBP <GGOBP@protonmail.ch>
2020-02-19 11:10:30 +0900

    check tests.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,10 +4,15 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=30)
     content = models.TextField()
+
     head_image = models.ImageField(upload_to='blog/%Y/%m/%d/', blank=True)
+
     created = models.DateTimeField()
     author = models.ForeignKey(User, on_delete=models.CASCADE,)
 
     def __str__(self):
         return '{} :: {}'.format(self.title, self.author)
+
+    def get_absolute_url(self):
+        return '/blog/{}/'.format(self.pk)
 
